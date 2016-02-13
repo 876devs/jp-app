5f6cedb87e5d68b5ff6f9d797189204b1021aad9
tremainebuchanan <tremainekbuchanan@gmail.com>
2016-02-13 17:34:16 -0500

    Search now optimized to locate records by first, middle and last name.

--- libs/lib.js
+++ libs/lib.js
@@ -2,30 +2,41 @@
  * Created by tremaine on 1/13/16.
  */
 (function(){
+
+    var KEYS = ['first_name', 'last_name', 'middle_name'];
+    /**
+     * Builds an or query
+     * @param list
+     * @returns {Array}
+     */
+    function buildOr(list){
+        var i = KEYS.length,
+            arr = [];
+        while(i--){
+            var query = {};
+            query[KEYS[i]] = {$in: list};
+            arr.push(query);
+        }
+        return arr;
+    }
+
+    function buildReqExp(term){
+        return new RegExp(term, 'i');
+    }
     /**
      * Builds query object from list of url query parameters.
      * @param queryList
      * @returns {{}}
      */
     exports.buildQuery = function(queryList){
-        var name = queryList.name.split(" "),
+        var list = queryList.name.split(" "),
             query = {};
-        switch(name.length){
-            case 1: query.first_name = new RegExp(name[0].toLowerCase(), 'i');
-                break;
-            case 2: query.first_name = new RegExp(name[0].toLowerCase(), 'i');
-                query.last_name = new RegExp(name[1].toLowerCase(), 'i');
-                break;
-        }
-
+            query = buildOr(list);
         if(queryList.parish){
             if(queryList.parish != "blank"){
                 query.parish = new RegExp(queryList.parish, 'i');
             }
         }
-        //comment
-        //another comment
         return query;
     };
-
 })();
